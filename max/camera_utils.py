029bcac7b9dafa51328f59cf27130fd769b7a909
Chris <christoph.rico@gmail.com>
2025-03-02 05:37:01 +0000

    Camera init, send/recv working

---
+++ max/camera_utils.py
@@ -0,0 +1,79 @@
+from picamera2.devices import IMX500
+import configparser
+from picamera2 import Picamera2
+from picamera2.devices.imx500 import NetworkIntrinsics, postprocess_nanodet_detection
+import numpy as np
+import cv2
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+# camera settings
+model = config['CAMERA']['model']
+height = int(config['CAMERA']['height'])
+width = int(config['CAMERA']['width'])
+iou = float(config['CAMERA']['iou'])
+threshold = float(config['CAMERA']['threshold'])
+max_detections = int(config['CAMERA']['max_detections'])
+inference_rate = int(config['CAMERA']['inference_rate'])
+
+
+def init_camera() -> tuple[Picamera2, IMX500]:
+    """
+    Initialize the camera and the IMX500 object
+    :return: tuple of Picamera2 and IMX500 objects
+    """
+    # This must be called before instantiation of Picamera2
+    imx500 = IMX500(model)
+    intrinsics = imx500.network_intrinsics
+    if not intrinsics:
+        intrinsics = NetworkIntrinsics()
+        intrinsics.task = "object detection"
+    elif intrinsics.task != "object detection":
+        print("Network is not an object detection task")
+        exit()
+
+    # Defaults
+    if intrinsics.labels is None:
+        with open("../assets/coco_labels.txt", "r") as f:
+            intrinsics.labels = f.read().splitlines()
+    intrinsics.update_with_defaults()
+
+    intrinsics.inference_rate = inference_rate
+
+    picam2 = Picamera2(imx500.camera_num)
+    config = picam2.create_preview_configuration(controls={"FrameRate": intrinsics.inference_rate}, buffer_count=12,
+                                                 main={"format": 'XRGB8888', "size": (width, height)})
+
+    imx500.show_network_fw_progress_bar()
+    picam2.start(config)
+
+    if intrinsics.preserve_aspect_ratio:
+        imx500.set_auto_aspect_ratio()
+
+    return picam2, imx500
+
+
+def get_num_people_local(metadata: dict, imx500: IMX500) -> int:
+    """
+    Get the number of people detected in the frame
+    :param metadata: metadata from the camera
+    :param imx500: IMX500 object
+    :return: number of people detected
+    """
+    np_outputs = imx500.get_outputs(metadata, add_batch=True)
+
+    if np_outputs is None:
+        return 0
+    
+    _, scores, classes = \
+        postprocess_nanodet_detection(outputs=np_outputs[0], conf=threshold, iou_thres=iou,
+                                        max_out_dets=max_detections)[0]
+    ## parse how many people are in the frame
+    num_persons = 0
+    for score, category in zip(scores, classes):
+        if score > threshold and category == 0:
+            num_persons += 1
+
+    return num_persons
+
---
+++ max/network_utils.py
@@ -0,0 +1,73 @@
+import socket
+import struct
+import msgpack
+import cv2
+import numpy as np
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+remote_ip = config["REMOTE"]["ip"]
+remote_port = int(config["REMOTE"]["port"])
+
+
+def init_connection() -> tuple[socket.socket, socket.socket]:
+    outgoing_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    outgoing_socket.connect((remote_ip, remote_port))
+
+    incoming_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    incoming_socket.bind(("0.0.0.0", remote_port))
+    incoming_socket.listen(1)
+    incoming_connection, _ = incoming_socket.accept()
+
+    return outgoing_socket, incoming_connection
+
+
+
+def send_frame(outgoing_socket: socket.socket, frame: np.ndarray, number_of_persons: int) -> None:
+    # Encode the frame as JPEG
+    _, frame_encoded = cv2.imencode('.jpg', frame)
+    frame_data = frame_encoded.tobytes()
+    
+    payload = msgpack.packb({'frame': frame_data, 'number_of_persons': number_of_persons}, use_bin_type=True)
+    
+    # Send the size of the data and then the payload
+    data_size = struct.pack("I", len(payload))
+    outgoing_socket.sendall(data_size + payload)
+
+
+def receive_frame(incoming_connection) -> tuple[np.ndarray, int]:
+    try:
+        # Receive the length of the incoming message
+        data_size = incoming_connection.recv(4)
+        if not data_size:
+            return None
+
+        # Unpack the length of the data
+        msg_len = struct.unpack("I", data_size)[0]
+
+        # Receive the actual data based on the received message length
+        data = b""
+        while len(data) < msg_len:
+            packet = incoming_connection.recv(msg_len - len(data))
+            if not packet:
+                break
+            data += packet
+
+        # Unpack the received data using msgpack
+        unpacked_data = msgpack.unpackb(data, raw=False)
+        frame_data = unpacked_data["frame"]
+        number_of_persons = unpacked_data["number_of_persons"]
+
+        # Decode the image from JPEG format back to a NumPy array
+        frame = cv2.imdecode(
+            np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_COLOR
+        )
+
+    except Exception as e:
+        print("Error:", e)
+        return None
+        
+    return frame, number_of_persons
